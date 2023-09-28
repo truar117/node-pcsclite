3cbfdc391fa63e8acecf162aac0e7cbdc50c130b
Martin Endler <pokusew@seznam.cz>
2023-09-28 02:05:20 +0200

    Make the module context-aware
    Credits:
    * https://github.com/santigimeno/node-pcsclite/pull/96
    * https://github.com/pokusew/node-pcsclite/pull/45
    * https://github.com/pokusew/node-pcsclite/pull/48
    
    Resources:
    * https://github.com/nodejs/nan/blob/v2.18.0/nan.h#L165
    * https://nodejs.org/api/addons.html#context-aware-addons

--- src/addon.cpp
+++ src/addon.cpp
@@ -6,4 +6,6 @@ void init_all(v8::Local<v8::Object> target) {
     CardReader::init(target);
 }
 
-NODE_MODULE(pcsclite, init_all)
+// see https://nodejs.org/api/addons.html#context-aware-addons
+// see https://github.com/nodejs/nan/blob/v2.18.0/nan.h#L165
+NAN_MODULE_WORKER_ENABLED(pcsclite, init_all)
